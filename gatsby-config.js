7a315a839e68c66aba3877fb877024b3a720f740
engie.matene <engie.matene@trademe.co.nz>
2021-03-19 19:25:12 +1300

    remove unused pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#000`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/HapaiFitnessNoOutline.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
